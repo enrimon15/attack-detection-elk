110821702ab4c654f2e0566314e8ee1cb2680d77
Enrico Monte <monte.enrico@libero.it>
2023-03-19 16:14:38 +0100

    start auditbeat

--- docker/test-metasploit/start.sh
+++ docker/test-metasploit/start.sh
@@ -3,4 +3,6 @@ docker-compose -f docker-compose-tomcat-pb.yml up -d
 sleep 5
 docker exec -it vulnerable-mts bash
 service packetbeat start -d -e --strict.perms=false
+#service auditbeat start -d -e --strict.perms=false
+sleep 3
 exit
