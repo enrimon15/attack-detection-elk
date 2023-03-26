de839e9627928ec0d1aafb1d6fa90a6c8738cae2
Enrico Monte <monte.enrico@libero.it>
2023-03-26 20:38:53 +0200

    auditbeat integration

--- docker/start.sh
+++ docker/start.sh
@@ -5,6 +5,8 @@ sleep 20
 docker exec -it vulnerable-mts bash
 # avvio packetbeat
 service packetbeat start -d -e --strict.perms=false
+service auditbeat start -d -e --strict.perms=false
+# to check if beat is running you can execute: service packetbeat status or service auditbeat status
 sleep 3
 # esco dal container e ritorno alla mia shell
 exit
