90472e0187fead85c14b36e41845deac030be7a7
Enrico Monte <monte.enrico@libero.it>
2023-04-19 12:17:41 +0200

    dashboards opensearch-dashboard

--- docker/test-metasploit/attack-scripts/ssh-connection.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod 600 "$1"
-ssh -i "$1" "root@$2"
-
-#example execution: ./ssh-connection.sh /root/myKey 172.30.0.6
--- docker/test-metasploit/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker-compose -f docker-compose-tomcat-pb.yml up -d
-sleep 5
-docker exec -it vulnerable-mts bash
-service packetbeat start -d -e --strict.perms=false
-#service auditbeat start -d -e --strict.perms=false
-sleep 3
-exit
