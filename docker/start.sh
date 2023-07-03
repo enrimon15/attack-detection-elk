215f08898b48e833e2aababe1d7b64a06638cded
Enrico Monte <monte.enrico@libero.it>
2023-07-03 16:49:51 +0200

    fix

--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 docker-compose up -d
-sleep 20
-# entro nel container vulnerabile ubuntu
-docker exec -it vulnerable-mts bash
+sleep 60
 # avvio packetbeat
-service packetbeat start -d -e --strict.perms=false
-service auditbeat start -d -e --strict.perms=false
-# to check if beat is running you can execute: service packetbeat status or service auditbeat status
-sleep 3
-# esco dal container e ritorno alla mia shell
-exit
+docker exec vulnerable-mts service packetbeat start -d -e --strict.perms=false
+docker exec vulnerable-mts service auditbeat start -d -e --strict.perms=false
+# to check if beat is running you can execute: docker exec vulnerable-mts service packetbeat status or service auditbeat status
+
+sleep 10
+
+# import dashboards
+curl -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" -k --form file=@config/kibana-dashboards/dashboards.ndjson
--- docker/test-metasploit/attack-scripts/meterpreter-access-session.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sessions -i "$1"
-
-# example execution: ./meterpreter-aceess-session.sh 1
-# 1 is session ID
